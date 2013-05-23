f7ced49fabc7043ab5775349770dbda981004d22
DBC-Apprentice <dbc@dbc.com>
2013-05-22 20:33:12 -0700

    OOJS

--- public/js/application.js
+++ public/js/application.js
@@ -1,93 +1,136 @@
-var bindKeyUps = function(){
-  var start = new Date()
-  $(document).on('keyup',function(event){
-    if(event.which == 65) {
-      $active = $('.active-1');
-      $active.next().append($('.active-1 img:first-child'));
-      $active.next().addClass('active-1');
-      $active.first().removeClass('active-1');
-        if($('td:last-child').hasClass("active-1")){
-        alert('Player 1 wins!');
-        alert('You took '  + Math.round((new Date() - start)/1000) + ' seconds!');
-        $(window).off();
-        $('#restart').show();
-        $('#newgame').show();
+Game = {
+  play: function() {
+    $('.columns').on("click",'img',function() {
+      $this = $(this);
+      var name = $this.attr('class');
+      var name_html = "<div class='selected-player'>" + name + "</div>";
+      $this.parent().prev().slideUp(3000);
+      $this.addClass("selected");
+      $this.siblings().hide();
+      $this.parent().removeClass('columns');
+      $this.animate({width:"100%", height:"100%"}, 3000);
+      $this.parent().append(name_html);
+      if ($('.selected').length == 2) {
+        $('.start-game-button').slideDown(3000);
+        $('#vs').show();
       }
-    }
-    if(event.which == 77) {
-      $active = $('.active-2');
-      $active.next().append($('.active-2 img:first-child'));
-      $active.next().addClass('active-2');
-      $active.first().removeClass('active-2');
-       if($('td:last-child').hasClass("active-2")){
-        alert('Player 2 wins');
-        alert('You took '  + Math.round((new Date() - start)/1000) + ' seconds!');
-        $(window).off();
-        $('#restart').show();
-        $('#newgame').show();
-      }
-    }
-  });
-};
+    });
 
-var moveToHome = function(){
-  $('#player1_strip td:first-child').addClass('active-1');
-  $('#player1_strip td:first-child').append($('.active-1 img:first-child'));
-  $('.active-1').last().removeClass('active-1');
-  $('#player2_strip td:first-child').addClass('active-2');
-  $('#player2_strip td:first-child').append($('.active-2 img:first-child'));
-  $('.active-2').last().removeClass('active-2');
-  $('#restart').hide();
-  $('#newgame').hide();
-};
-
-    $(document).ready(function() {
-      $('.columns').on("click",'img',function(){
-        $this = $(this);
-        var name = $this.attr('class');
-        var name_html = "<div class='selected-player'>" + name + "</div>"
-        $this.parent().prev().slideUp(3000);
-        $this.addClass("selected");
-        $this.siblings().hide();
-        $this.parent().removeClass('columns');
-        $this.animate({width:"100%", height:"100%"}, 3000);
-        $this.parent().append(name_html)
-    // $parent_div = console.log($($this.parent()));
-    if($('.selected').length == 2)
-    {
-      $('.button').slideDown(3000);
-      $('#vs').show();
-    }
-  });
-
-  // $(".button").on("click", function(){
-  //   var $selected = $('.selected') 
-  //   window.location.href = "/game?p1=" + $selected[0]["src"] + "&p2=" + $selected[1]["src"];
-  // });
-
-    $(".button").on("click", function(){
-      $(".button").hide();
+    $(".start-game-button").on("click", function() {
+      $(".start-game-button").hide();
       var $selected = $('.selected');
       $selected.hide();
       $('.selected-player').hide();
       $('#vs').hide();
       $('racer_table').show();
-      $('.active-1').html($selected[0]);
-      $('.active-2').html($selected[1]);
+      addPhoto($selected);
       $selected.show();
-      bindKeyUps();
-  });
-
-    $("#restart").on("click", function(){
       moveToHome();
     });
 
-    $('#newgame').on("click", function(){
-      window.location.href = '/'
-    });
+      // function addPhoto() {
+      // $('.active-1').html($selected[0]).find('img').attr('id', 'player1');
+      // $('.active-2').html($selected[1]).find('img').attr('id', 'player2');
+      // $('#player1').animate({width:"50px", height:"50px"},1000);
+      // $('#player2').animate({width:"50px", height:"50px"},1000);
 
-    $(".nate").hover(function(){
-      $(this).replaceWith('<img class="nate" src="http://i3.ytimg.com/vi/FzF5nf1g14c/hqdefault.jpg">');
-    });
 
-});
+
+      function addPhoto(selected) {
+        for (var playerNumber = 1 ; playerNumber <= 2; playerNumber ++) {
+          var className = 'active-' + playerNumber;
+          var playerName = 'player' + playerNumber;
+          $('.' + className).html(selected[playerNumber - 1]).find('img').attr('id', playerName);
+          $('#' + playerName).animate({width:"50px", height:"50px"},1000);
+        }
+      }
+
+      function bindKeyUps() {
+        $(document).on('keyup', function(event){
+          if (Game.isOver()) { return; console.log("Sup bro"); }
+          else{
+            if (event.which == 65) {
+              movePlayer(1);
+            }
+            if (event.which == 77) {
+              movePlayer(2);
+            }
+          }
+        });
+
+        function movePlayer(playerNumber) {
+          var className = 'active-' + playerNumber;
+          var playerName = '#player' + playerNumber;
+          $active = $('.' + className);
+          $active.next().append($(playerName));
+          $active.removeClass(className).next().addClass(className);
+        }
+      }
+
+      function moveToHome() {
+        var count = 3;
+        var countdown = setInterval(function() {
+          if (count === 0){
+            bindKeyUps();
+            clearInterval(countdown);
+            $('#countdown').text("Go!");
+            start = new Date();
+          }
+          else{
+            count --;
+            $('#countdown').text("Start in " + count);
+          }
+        }, 1000);
+        $('td').removeClass();
+        $('#player1_strip td:first-child').addClass('active-1');
+        $('.active-1').append($('#player1'));
+        $('#player2_strip td:first-child').addClass('active-2');
+        $('.active-2').append($('#player2'));
+
+      }
+
+      $("#restart").on("click", function(){
+        $('.game-end-buttons').css("display", "none");
+        moveToHome();
+        countdown;
+      });
+
+      $('#newgame').on("click", function(){
+        $('.game-end-buttons').css("display", "none");
+        window.location.href = '/';
+      });
+
+      $(".nate").hover(function(){
+        $(this).replaceWith('<img class="nate" src="http://i3.ytimg.com/vi/FzF5nf1g14c/hqdefault.jpg">');
+      });
+    },
+
+    isOver: function() {
+      function didPlayerWin(playerNumber) {
+        var className = 'active-' + playerNumber;
+        if($('td:last-child').hasClass(className)){
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      function declareWinner(playerNumber) {
+        $(document).unbind('keyup')
+        alert('Player ' + playerNumber + ' wins!');
+        alert('You took '  + Math.round((new Date() - start)/1000) + ' seconds!');
+        $('.game-end-buttons').css("display", "block");
+      }
+      for(var playerNumber = 1; playerNumber <=2; playerNumber++) {
+        if(didPlayerWin(playerNumber)) {
+          declareWinner(playerNumber);
+        }
+      }
+    }
+  };
+
+  $(document).ready(function() {
+    Game.play();
+    $(document)
+    Game.bindKeyUps
+  });
